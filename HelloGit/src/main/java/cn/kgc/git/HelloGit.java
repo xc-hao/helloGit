32eb1bf9ee220110decf027004852e1d6b2aa5fa
xc-hao <1248941791@qq.com>
2020-06-09 09:55:53 +0800

    SHH上传

--- HelloGit/src/main/java/cn/kgc/git/HelloGit.java
+++ HelloGit/src/main/java/cn/kgc/git/HelloGit.java
@@ -12,5 +12,6 @@ public class HelloGit {
         System.out.println("====在线更新====");
         System.out.println("====克隆的项目====");
 	System.out.println("====命令行上传====");
+	System.out.println("====SSH命令行上传====");
     }
 }
